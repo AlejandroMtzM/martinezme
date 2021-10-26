e2869adad12af2bfa5e6756280d57db83b12ef12
Marcos Alejandro Martinez Mendoza <54965562+AlejandroMtzM@users.noreply.github.com>
2021-10-26 09:51:01 -0500

    Threads
    Tópicos

---
+++ Threads/Threads/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace Threads
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblHora = new System.Windows.Forms.Label();
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.pbProgreso = new System.Windows.Forms.ProgressBar();
+            this.numCargaUCP = new System.Windows.Forms.NumericUpDown();
+            this.lblPorcentaje = new System.Windows.Forms.Label();
+            this.Temporalizador = new System.Windows.Forms.Timer(this.components);
+            this.hiloTrabajador = new System.ComponentModel.BackgroundWorker();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numCargaUCP)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblHora.Location = new System.Drawing.Point(56, 21);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(104, 21);
+            this.lblHora.TabIndex = 0;
+            this.lblHora.Text = "00:00:00am";
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Location = new System.Drawing.Point(113, 102);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(75, 23);
+            this.btnEjecutar.TabIndex = 1;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            this.btnEjecutar.Click += new System.EventHandler(this.btnEjecutar_Click);
+            // 
+            // pbProgreso
+            // 
+            this.pbProgreso.Location = new System.Drawing.Point(31, 73);
+            this.pbProgreso.Name = "pbProgreso";
+            this.pbProgreso.Size = new System.Drawing.Size(157, 23);
+            this.pbProgreso.TabIndex = 2;
+            // 
+            // numCargaUCP
+            // 
+            this.numCargaUCP.Increment = new decimal(new int[] {
+            50000,
+            0,
+            0,
+            0});
+            this.numCargaUCP.Location = new System.Drawing.Point(40, 131);
+            this.numCargaUCP.Maximum = new decimal(new int[] {
+            705032704,
+            1,
+            0,
+            0});
+            this.numCargaUCP.Name = "numCargaUCP";
+            this.numCargaUCP.Size = new System.Drawing.Size(120, 23);
+            this.numCargaUCP.TabIndex = 3;
+            this.numCargaUCP.Value = new decimal(new int[] {
+            50000,
+            0,
+            0,
+            0});
+            // 
+            // lblPorcentaje
+            // 
+            this.lblPorcentaje.AutoSize = true;
+            this.lblPorcentaje.Location = new System.Drawing.Point(156, 55);
+            this.lblPorcentaje.Name = "lblPorcentaje";
+            this.lblPorcentaje.Size = new System.Drawing.Size(32, 15);
+            this.lblPorcentaje.TabIndex = 4;
+            this.lblPorcentaje.Text = "0.0%";
+            // 
+            // Temporalizador
+            // 
+            this.Temporalizador.Enabled = true;
+            this.Temporalizador.Interval = 1;
+            this.Temporalizador.Tick += new System.EventHandler(this.Temporalizador_Tick);
+            // 
+            // hiloTrabajador
+            // 
+            this.hiloTrabajador.WorkerReportsProgress = true;
+            this.hiloTrabajador.WorkerSupportsCancellation = true;
+            this.hiloTrabajador.DoWork += new System.ComponentModel.DoWorkEventHandler(this.hiloTrabajador_DoWork);
+            this.hiloTrabajador.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.hiloTrabajador_ProgressChanged);
+            this.hiloTrabajador.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.hiloTrabajador_RunWorkerCompleted);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(31, 102);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancelar";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(216, 185);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.lblPorcentaje);
+            this.Controls.Add(this.numCargaUCP);
+            this.Controls.Add(this.pbProgreso);
+            this.Controls.Add(this.btnEjecutar);
+            this.Controls.Add(this.lblHora);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.numCargaUCP)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.ProgressBar pbProgreso;
+        private System.Windows.Forms.NumericUpDown numCargaUCP;
+        private System.Windows.Forms.Label lblPorcentaje;
+        private System.Windows.Forms.Timer Temporalizador;
+        private System.ComponentModel.BackgroundWorker hiloTrabajador;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ Threads/Threads/Form1.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Threads
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Temporalizador_Tick(object sender, EventArgs e)
+        {
+            lblHora.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private Thread hiloSecundario;
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            //btnEjecutar.Enabled = false;
+            //numCargaUCP.Enabled = false;
+            //pbProgreso.Value = 0;
+            //ThreadStart DelegadoPS = new ThreadStart(TareaSecundariaMethodInvoker);
+            //hiloSecundario = new Thread(DelegadoPS);
+            //hiloSecundario.Start();
+
+            ////Metodo utilizando componente backgroundworker
+
+            btnEjecutar.Enabled = false;
+            numCargaUCP.Enabled = false;
+            pbProgreso.Value = 0;
+
+            //hiloTrabajador.RunWorkerAsync();
+
+            //controladorParaHiloSecundario = new ManualResetEvent(false);
+            //controladorHiloSecundarioParado = new ManualResetEvent(false);
+
+            //hiloSecundario = new Thread(TareaSecundariaControlHilos);
+            //hiloSecundario.Start();
+        }
+
+        //Metodo OBJETO THREAD
+        private delegate void SetValueDelegateInt(int value);
+        //private void SetValue_pbProgreso(int hecho)
+        //{
+        //    if (pbProgreso.InvokeRequired)
+        //    {
+        //        SetValueDelegateInt delegado = new SetValueDelegateInt(SetValue_pbProgreso);
+        //        pbProgreso.Invoke(delegado, new object[] { hecho });
+        //    }
+        //    else
+        //    {
+        //        pbProgreso.Value = hecho;
+        //        lblPorcentaje.Text = hecho.ToString()+"%";
+        //    }
+        //}
+
+        private delegate void SetEnableDelegateBool(bool value);
+        private void SetValue_btnEjecutar(bool hecho)
+        {
+            if (btnEjecutar.InvokeRequired)
+            {
+                SetEnableDelegateBool delegado = new SetEnableDelegateBool(SetValue_btnEjecutar);
+                pbProgreso.Invoke(delegado, new object[] { hecho });
+            }
+            else
+            {
+                btnEjecutar.Enabled = hecho;
+            }
+        }
+        private void SetValue_numCargaUCP(bool hecho)
+        {
+            if (numCargaUCP.InvokeRequired)
+            {
+                SetEnableDelegateBool delegado = new SetEnableDelegateBool(SetValue_numCargaUCP);
+                numCargaUCP.Invoke(delegado, new object[] { hecho });
+            }
+            else
+            {
+                numCargaUCP.Enabled = hecho;
+            }
+        }
+
+        private void TareaSecundariaThread()
+        {
+            int hecho = 0, tpHecho = 0;
+
+            while (hecho < numCargaUCP.Value)
+            {
+                hecho += 1;
+                tpHecho = (int)(hecho / numCargaUCP.Value * 100);
+
+                if (tpHecho>pbProgreso.Value)
+                {
+                    SetValue_pbProgreso(tpHecho);
+                }
+            }
+
+            SetValue_btnEjecutar(true);
+            SetValue_numCargaUCP(true);
+
+            MessageBox.Show("Metodo utilizado threads");
+        }
+
+
+
+        //Metodo OBJERTO METHODINVOKER
+        private void SetEnabled_btnEjecutar()
+        {
+            btnEjecutar.Enabled = true;
+        }
+
+        private void SetEnabled_numCargaUCP()
+        {
+            numCargaUCP.Enabled = true;
+        }
+
+        private void SetValue_pbProgreso()
+        {
+            pbProgreso.Value = tpHecho;
+            lblPorcentaje.Text = tpHecho.ToString() + "%";
+        }
+
+        public int tpHecho = 0;
+        private void TareaSecundariaMethodInvoker()
+        {
+            MethodInvoker delegado;
+
+            int hecho = 0;
+            tpHecho = 0;
+            delegado = new MethodInvoker(SetValue_pbProgreso);
+
+            while (hecho < numCargaUCP.Value)
+            {
+                hecho += 1;
+                tpHecho = (int)(hecho / numCargaUCP.Value * 100);
+
+                if (tpHecho > pbProgreso.Value)
+                {
+
+                    pbProgreso.Invoke(delegado);
+
+                }
+
+            }
+
+            delegado = new MethodInvoker(SetEnabled_btnEjecutar);
+            btnEjecutar.Invoke(delegado);
+
+            delegado = new MethodInvoker(SetEnabled_numCargaUCP);
+            numCargaUCP.Invoke(delegado);
+
+            MessageBox.Show("Metodo utilizado OBJETO METHODINVOKER");
+        }
+
+
+
+        //Metodo utilizando componente Backgrounworker
+        private void hiloTrabajador_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker hiloTr = (BackgroundWorker)sender;
+            TareaSecundariaBGW(hiloTr, e);
+        }
+
+        private void TareaSecundariaBGW(BackgroundWorker hiloTR,DoWorkEventArgs e)
+        {
+            int hecho = 0, tpHecho = 0;
+
+            while (hecho <numCargaUCP.Value)
+            {
+                hecho += 1;
+
+                tpHecho = (int)(hecho / numCargaUCP.Value * 100);
+
+                if (tpHecho > pbProgreso.Value)
+                {
+                    hiloTR.ReportProgress(tpHecho);
+                }
+
+                //REALIZAR CANCELACION DE LA OPERACION
+
+                if (hiloTR.CancellationPending)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                if (hiloTR.CancellationPending)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+            }
+            MessageBox.Show("Metodo utilizando componente backgroundWorker");
+        }
+
+        private void hiloTrabajador_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pbProgreso.Value = e.ProgressPercentage;
+            lblPorcentaje.Text = e.ProgressPercentage.ToString() + "%";
+        }
+
+        private void hiloTrabajador_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message);
+            }
+            else if(e.Cancelled)
+            {
+                MessageBox.Show("Operacion cancelada");
+                btnEjecutar.Enabled = true;
+                numCargaUCP.Enabled = true;
+            }
+            else
+            {
+                btnEjecutar.Enabled = true;
+                numCargaUCP.Enabled = true;
+               
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //hiloTrabajador.CancelAsync();
+            //ParaHiloSecundario();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            //hiloTrabajador.CancelAsync();
+            //ParaHiloSecundario();
+
+        }
+
+        //METODO DETENER HILO DE FORMA CONTROLADA
+
+        //private ManualResetEvent controladorParaHiloSecundario;
+        //private ManualResetEvent controladorHiloSecundarioParado;
+
+        //private delegate void SetValueDlegate(int prValue);
+        //private delegate void SetEnabledDelegate(bool prEnabled);
+
+        //private void SetValue_pbProgreso(int hecho)
+        //{
+        //    if (pbProgreso.InvokeRequired)
+        //    {
+        //        SetValueDlegate delegado = new SetValueDlegate(SetValue_pbProgreso);
+        //        pbProgreso.Invoke(delegado, new object[] { hecho });
+        //        lblPorcentaje.Invoke(delegado, new object[] { hecho });
+        //    }
+        //    else
+        //    {
+        //        pbProgreso.Value = hecho;
+        //        lblPorcentaje.Text = hecho.ToString() + "%";
+        //    }
+        //}
+
+        //private void SetEnabled_btnCalcular(bool b)
+        //{
+        //    if (btnEjecutar.InvokeRequired)
+        //    {
+        //        SetEnabledDelegate delegado = new SetEnabledDelegate(SetEnabled_btnCalcular);
+        //        btnEjecutar.Invoke(delegado, new object[] { b });
+        //    }
+        //    else
+        //    {
+        //        btnEjecutar.Enabled = b;
+        //    }
+        //}
+
+        //private void SetEnabled_numCarga(bool b)
+        //{
+        //    if (numCargaUCP.InvokeRequired)
+        //    {
+        //        SetEnabledDelegate delegado = new SetEnabledDelegate(SetEnabled_numCarga);
+        //        numCargaUCP.Invoke(delegado, new object[] { b });
+        //    }
+        //    else
+        //    {
+        //        numCargaUCP.Enabled = b;
+        //    }
+        //}
+
+        //private bool ParaHiloSecundario()
+        //{
+        //    if (hiloSecundario == null || hiloSecundario.IsAlive)
+        //    {
+        //        return false;
+        //    }
+
+        //    controladorParaHiloSecundario.Set();
+
+        //    while (hiloSecundario.IsAlive)
+        //    {
+        //        WaitHandle.WaitAll((new ManualResetEvent[] {controladorHiloSecundarioParado}),100,false);
+        //        Application.DoEvents();
+        //    }
+
+        //    hiloSecundario = null;
+        //    return true;
+            
+        //}
+        //private void TareaSecundariaControlHilos()
+        //{
+        //    int hecho = 0, tpHecho = 0;
+
+        //    while (hecho < numCargaUCP.Value)
+        //    {
+        //        hecho += 1;
+        //        tpHecho = (int)(hecho / numCargaUCP.Value * 100);
+
+        //        if (tpHecho > pbProgreso.Value)
+        //        {
+        //            SetValue_pbProgreso(tpHecho);
+        //        }
+        //        if (controladorParaHiloSecundario.WaitOne(0,false))
+        //        {
+        //            controladorHiloSecundarioParado.Set();
+        //            break;
+        //        }
+        //    }
+
+        //    SetEnabled_btnCalcular(true);
+        //    SetEnabled_numCarga(true);
+
+        //    MessageBox.Show("Metodo controlado");
+        //}
+
+    }
+    
+}
